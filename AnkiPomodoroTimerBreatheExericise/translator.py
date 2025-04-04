e5551003cd8c9e5e26856fab5a451b6a958cb270
ooopus <opuses114514@gmail.com>
2025-04-04 10:10:20 +0900

    refactor(translator): simplify locale handling and add i18n script
    Remove redundant `get_locale` function and directly use `lang` for translation.
    Add `replace.py` script to automate wrapping strings in `_()` for i18n support.

--- AnkiPomodoroTimerBreatheExericise/translator.py
+++ AnkiPomodoroTimerBreatheExericise/translator.py
@@ -4,11 +4,6 @@ from aqt import QLocale
 
 lang = QLocale.system().name()  # like "zh_CN"
 
-
-def get_locale():
-    """Returns the current locale."""
-    return getattr(QLocale.system().name(), lang, "zh")
-
 localedir = os.path.join(os.path.dirname(__file__), './locales')
-translation = gettext.translation('messages', localedir,languages=get_locale(), fallback=True)
+translation = gettext.translation('messages', localedir, languages=lang, fallback=True)
 _ = translation.gettext
